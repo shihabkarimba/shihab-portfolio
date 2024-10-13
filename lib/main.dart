ce3cd9fc699e275195732d45dd8a079ba08bf15b
Shihabudheen <shihabudheeninfo@gmail.com>
2024-10-13 22:22:20 +0530

    title - home page

--- lib/main.dart
+++ lib/main.dart
@@ -11,13 +11,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      title: 'Shihab Portfolio',
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
